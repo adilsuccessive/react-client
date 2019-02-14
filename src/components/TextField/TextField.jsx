d91c4998bed78ad3da74a33c954f89712afa465b
adilsuccessive <mohammad.adil@successive.tech>
2019-02-14 19:38:36 +0530

    feat: add files for textfields

---
+++ src/components/TextField/TextField.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+const textField = () => {
+  return <input type = "text" />
+}
+export default textField;
---
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  { TextField } from "../../components"
+
+const Demo = () => {
+   return (<TextField/>)
+  }
+
+
+export default Demo;
+;
