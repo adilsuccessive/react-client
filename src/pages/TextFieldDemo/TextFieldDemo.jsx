6fad41eaaa009ceae0f1aa44db0b4a7d590bb681
adilsuccessive <mohammad.adil@successive.tech>
2019-02-19 13:12:07 +0530

    feat: 🎸 create the slider component and use in textFieldDemo

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { TextField } from '../../components';
+import { PUBLIC_IMAGE_FOLDER } from '../../configs/constants';
+import { TextField, Slider } from '../../components';
 
+const Banners = [
+  `${PUBLIC_IMAGE_FOLDER}default.png`,
+  `${PUBLIC_IMAGE_FOLDER}cloud.jpg`,
+  `${PUBLIC_IMAGE_FOLDER}dns-server.png`,
+  `${PUBLIC_IMAGE_FOLDER}full-stack-web-development.jpg`,
+  `${PUBLIC_IMAGE_FOLDER}js.jpg`,
+  `${PUBLIC_IMAGE_FOLDER}load-balancer.png`,
+
+];
 const TextFieldDemo = () => (
   <div>
+    <div style={{ textAlign: 'center' }}>
+      <Slider banners={Banners} />
+    </div>
     <h4>This is a disabled input</h4>
     <TextField placeholder="Disabled Input" disabled />
     <h4>A Valid Input</h4>
